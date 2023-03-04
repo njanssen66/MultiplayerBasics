8aa15dfd27b68a69782e5c81e45782026eef076e
njanssen66 <82132278+njanssen66@users.noreply.github.com>
2023-03-04 15:58:25 +1100

    Created MyNetworkManager

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override OnClientConnect()
+    {
+        
+    }
+}
