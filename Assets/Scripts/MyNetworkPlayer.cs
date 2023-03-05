a964ff19833d1b8d7c109af51e5c8a1c32224295
njanssen66 <82132278+njanssen66@users.noreply.github.com>
2023-03-05 14:16:23 +1100

    Added ui for name and color for players

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -1,10 +1,21 @@
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar] [SerializeField] private string displayName = "Missing name";
-    [SyncVar] [SerializeField] private Color displayColor = Color.black;
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColorRenderer;
+
+    [SyncVar(hook=nameof(HandleDisplayNameUpdated))] 
+    [SerializeField] 
+    private string displayName = "Missing name";
+
+    [SyncVar(hook=nameof(HandleDisplayColorUpdated))] 
+    [SerializeField] 
+    private Color displayColor = Color.black;
+
+#region Server
 
     [Server]
     public void SetDisplayName(string newDisplayName)
@@ -17,4 +28,40 @@ public class MyNetworkPlayer : NetworkBehaviour
     {
         displayColor = newDisplayColor;
     }
+
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        //Can do server validation here
+        SetDisplayName(newDisplayName);
+        RpcLogName(newDisplayName);
+    }
+
+#endregion
+
+#region Client
+
+    private void HandleDisplayColorUpdated(Color oldColor, Color newColor)
+    {
+        displayColorRenderer.material.SetColor("_Color", newColor);
+    }
+
+    private void HandleDisplayNameUpdated(string oldName, string newName)
+    {
+        displayNameText.SetText(newName);
+    }
+
+    [ContextMenu("Set My Name")]
+    private void SetMyName()
+    {
+        CmdSetDisplayName("My New Name");
+    }
+
+    [ClientRpc]
+    private void RpcLogName(string newName)
+    {
+        Debug.Log($"A player had their name updated to {newName}");
+    }
+
+#endregion
 }
